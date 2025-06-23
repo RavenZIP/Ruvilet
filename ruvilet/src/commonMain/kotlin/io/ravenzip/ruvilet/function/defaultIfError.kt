8d4dba9bf8d21af29d67833c74caac6647f8fd12
Черных Александр <razor01478920@mail.ru>
2025-06-23 20:58:01 +0300

    Доработана сигнатура метода defaultIfError

--- ruvilet/src/commonMain/kotlin/io/ravenzip/ruvilet/function/defaultIfError.kt
+++ ruvilet/src/commonMain/kotlin/io/ravenzip/ruvilet/function/defaultIfError.kt
@@ -1,8 +1,7 @@
 package io.ravenzip.ruvilet.function
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.catch
 
-fun <T> Flow<T>.defaultIfError(transform: suspend FlowCollector<T>.() -> T): Flow<T> =
-    this.catch { emit(transform()) }
+inline fun <reified T> Flow<T>.defaultIfError(recoveryValue: T): Flow<T> =
+    this.catch { emit(recoveryValue) }
