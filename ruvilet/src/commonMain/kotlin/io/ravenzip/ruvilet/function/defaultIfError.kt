f00f829816ddc3a7ac5dcda8526aca27fc826a11
Черных Александр <razor01478920@mail.ru>
2025-06-23 21:53:19 +0300

    Документация для операторов перехвата ошибок

--- ruvilet/src/commonMain/kotlin/io/ravenzip/ruvilet/function/defaultIfError.kt
+++ ruvilet/src/commonMain/kotlin/io/ravenzip/ruvilet/function/defaultIfError.kt
@@ -3,5 +3,23 @@ package io.ravenzip.ruvilet.function
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 
+/**
+ * Catches errors in the flow and replaces them with the specified recovery value.
+ *
+ * Example:
+ * ```
+ * flow {
+ *     emit(1)
+ *     emit(2)
+ *     throw Exception("Error")
+ * }.defaultIfError(-1)
+ * .collect { println(it) }  //Output: 1, 2, -1
+ * ```
+ *
+ * @param recoveryValue the value to be emitted when an error occurs
+ * @return a new [Flow] that will emit [recoveryValue] instead of throwing an exception when an
+ *   error occurs
+ * @see [Русская документация](docs/ru/defaultIfError.md)
+ */
 inline fun <reified T> Flow<T>.defaultIfError(recoveryValue: T): Flow<T> =
     this.catch { emit(recoveryValue) }
--- ruvilet/src/commonMain/kotlin/io/ravenzip/ruvilet/function/ignoreError.kt
+++ ruvilet/src/commonMain/kotlin/io/ravenzip/ruvilet/function/ignoreError.kt
@@ -5,4 +5,20 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.emptyFlow
 
+/**
+ * Catches errors in the flow and terminates the stream on error without emitting any values.
+ *
+ * Example:
+ * ```
+ * flow {
+ *     emit(1)
+ *     emit(2)
+ *     throw RuntimeException("Error")
+ * }.ignoreError()
+ * .collect { println(it) }  // Output: 1, 2
+ * ```
+ *
+ * @return a new [Flow] that completes normally on error
+ * @see [Русская документация](docs/ru/ignoreError.md)
+ */
 fun <T> Flow<T>.ignoreError(): Flow<T> = this.catch { emitAll(emptyFlow()) }
